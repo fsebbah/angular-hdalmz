77a0a9ee73a8aef2014d6e2a8f9ea6464bd9b579
fsebbah <fsebbah@cofas-edition.com>
2019-07-14 21:29:21 +0200

    4 files modified

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -3,7 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
+  /*
+  template:
+  `
+    <h1>Franck Sebbah Custom Template</h1>
+    <style>
+    h1{
+      color:green;
+    }
+    </style>
+  `
+  ,
+  */
   styleUrls: ['./welcome.component.css']
+ /* styles:[`
+  p{
+    color:blue;
+  }
+  `]
+  */
 })
 export class WelcomeComponent implements OnInit {
 
