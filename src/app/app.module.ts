b528ee4c8667932937323f281137c345099bdf7b
fsebbah <fsebbah@cofas-edition.com>
2019-07-15 13:01:47 +0200

    2 files modified, 9 added, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {AppRoutingModule} from './app-routing.module';
-
+import {AdminModule} from './admin/admin.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import {WelcomeComponent} from './welcome/welcome.component';
-import { LoginComponent } from './login/login.component';
-import { ColorPipe } from './welcome/color.pipe';
-import { HighlightDirective } from './welcome/highlight.directive';
-import { UnlessDirective } from './welcome/unless.directive'
+
+
+//import { HelloComponent } from './hello.component';
+//import {WelcomeComponent} from './welcome/welcome.component';
+//import { LoginComponent } from './login/login.component';
+//import { ColorPipe } from './welcome/color.pipe';
+//import { HighlightDirective } from './welcome/highlight.directive';
+//import { UnlessDirective } from './welcome/unless.directive'
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,FormsModule,AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent,WelcomeComponent, LoginComponent, ColorPipe, HighlightDirective, UnlessDirective ],
+  imports:      [ BrowserModule, FormsModule,FormsModule,AppRoutingModule,AdminModule ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
