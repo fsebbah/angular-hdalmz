f196cef01330594f624e73180aea0dfbe35c6d67
fsebbah <fsebbah@cofas-edition.com>
2019-07-16 11:32:36 +0200

    3 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   imports:      [ BrowserModule,AppRoutingModule ],
   providers: [],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, AboutComponent, HomeComponent ],
   bootstrap:    [ AppComponent ],
 
 })
