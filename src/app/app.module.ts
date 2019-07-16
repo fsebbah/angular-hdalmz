c611c6438e5d4bff1a135237d6ac36e5c0466a19
fsebbah <fsebbah@cofas-edition.com>
2019-07-16 11:21:54 +0200

    2 files modified, 20 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
-import {AdminModule} from './admin/admin.module';
 import { AppComponent } from './app.component';
-import {WelcomeService} from './admin/welcome/welcome.service';
 
 
 @NgModule({
-  imports:      [ BrowserModule,AppRoutingModule,AdminModule ],
-  providers: [WelcomeService],
+  imports:      [ BrowserModule,AppRoutingModule ],
+  providers: [],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ],
 
