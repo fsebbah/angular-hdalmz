6babbd2fa332d33f3fee032d8bc59748d1257c67
fsebbah <fsebbah@cofas-edition.com>
2019-07-16 13:06:27 +0200

    1 file modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { WorkHistoryComponent } from './about/work-history/work-history.component';
+import { UsersComponent } from './users/users.component';
+import { UsersResolverService } from './users-resolver.service';
+import { SingleUserComponent } from './single-user/single-user.component';
+import { SingleUserResolverService } from './single-user-resolver.service';
 
 
 @NgModule({
   imports:      [ BrowserModule,AppRoutingModule ],
-  providers: [],
-  declarations: [ AppComponent, AboutComponent, HomeComponent, WorkHistoryComponent ],
+  providers: [UsersResolverService, SingleUserResolverService],
+  declarations: [ AppComponent, AboutComponent, HomeComponent, WorkHistoryComponent, UsersComponent, SingleUserComponent ],
   bootstrap:    [ AppComponent ],
 
 })
